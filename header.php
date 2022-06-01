eabecd7906985dab7cebeb631e3804b0df28f848
duongthanhcongminh <katminh2910@gmail.com>
2022-06-01 20:19:18 +0700

    minh

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dovile Social Media Influencer Business Website Template</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css"> 
+</head>
+<body>
+    <div class="container">
+        <div class="row bg-white border-bottom fixed-top header">
+            <div class="nav justify-content-center">
+                <a class="fs-1 fw-semibold nav-link text-muted  " href="#">DOVILE</a>
+            </div>
+        <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active fw-light text-decoration-underline text-muted fs-5" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active fw-light text-decoration-underline text-muted fs-5" href="#">SERVICES</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active fw-light text-decoration-underline text-muted fs-5" href="#">ABOUT ME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active fw-light text-decoration-underline text-muted fs-5" href="#">CONTACT</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active fw-light text-decoration-underline text-muted fs-5" href="#">BLOG</a>
+        </li>
+        </ul>
+        </div>
+    </div>
+    
---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+include "./header.php";
+include "./footer.php"
+?>
+
+ <div class="intro">
+     <div class="container-fluid block-1">
+         <div class="container">
+             <div class="row align-items-center justify-content-between">
+                 <div class="col-5">
+                   
+                         <p class=" intro-text">Hello, so good to see you!</p>
+                         <p class="fw-light fs-5 mt-5"> I help small and medium-sized businesses grow their audience and brand recognition through social media.</p>
+
+                    <button type="button" class="btn btn-dark btn-lg mt-5">Contact me</button></div>
+
+                 <div class="col-5">
+                     <img src="./image/ava.avif" alt="">
+                 </div>
+             </div>
+         </div>
+     </div>
+     <div class="container-fluid block-2">
+         <div class="container">
+             <div class="row align-items-center">
+                 <div class="col-3 fs-1">Featured</div>
+                 <div class="col-3 fs-1">Lani Haw</div>
+                 <div class="col-3 fs-1">Deon Five</div>
+                 <div class="col-3 fs-1">KNOXEMUS</div>
+             </div>
+         </div>
+     </div>
+     <div class="container-fluid block-3">
+         <div class="container">
+             <div class="row d-flex justify-content-center align-items-center">
+                 <div class="col-7 ">
+            
+                         <p class="intro-text text-center">My name is Dovile</p>
+                         <p class="fw-light fs-5 text-center">I'm your new social media bestie. My mission is to help small and medium-sized businesses grow their audience and brand recognition through social media. To do that, I believe it's paramount to create a committed and genuine community that has a positive influence on people’s lives beyond the online sphere. My goal is to help you understand the power of social media and create connections that go beyond the internet.</p>
+
+
+                 </div>
+             </div>
+         </div>
+     </div>
+     <div class="container-fluid block-4">
+         <div class="container">
+             <div class="row align-items-center justify-content-between">
+                <div class="col-5">
+                     <img src="./image/ava2.avif" alt="">
+                 </div> 
+                 <div class="col-5">
+                   
+                         <p class=" intro-text">Services</p>
+                         <p class="fw-light fs-5 mt-5">From campaign planning and consultation to content creation and extended social media campaigning, I'm here to help your brand sound like you online.</p>
+
+                    <button type="button" class="btn btn-dark btn-lg mt-5">Read more</button></div>
+             </div>
+         </div>
+     </div>
+     <div class="container-fluid block-5">
+         <div class="container">
+            <div class="row d-flex justify-content-center align-items-center">
+                 <div class="col-7 ">           
+                         <p class="intro-text text-center">Are you ready to...</p>                
+                 </div>
+             </div>
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-6">
+                    <p>It's not enough to just create an account and post something. Your content needs to represents your brand and showcase your mission and values. And you want to do it in a way that stops the user in their tracks – with beautiful content.</p>
+                </div>
+                <div class="col-6">
+                    <p>Selling online is different from traditional sales channels. But most of all, you need to gain your audience's trust if you want them to part with their dollar.</p>
+                </div>
+                <div class="col-6">
+                    <p>Everybody is online, so why shouldn't you try to reach out to them? With a bigger reach than ever before, the world really is your oyster when it comes to selling online.</p>
+                </div>
+                <div class="col-6">
+                    <p>Social media marketing is much more than simply posting about an upcoming sale. With the right approach, you can exponentially grow your bottom line.</p>
+                </div>
+         </div>
+     </div>
+ </div>
