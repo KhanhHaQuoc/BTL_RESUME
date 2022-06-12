23882e07bc31d2477177072de528680f5165b166
duongthanhcongminh <katminh2910@gmail.com>
2022-06-12 15:57:16 +0700

    Minh update admin

---
+++ admin/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css"> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+</head>
+<body>
+    <main class="form-signin bg-secondary">
+        <form method="POST" action="signin.php" class="bg-white">
+            <a class="mx-auto text-decoration-none text-body" style="font-size:75px" href="./">MIKHA</a>
+            <h1 class="h3 mb-3 fw-bold">Please sign in</h1>
+
+            <div class="form-floating mb-3 w-100">
+            <input type="text" class="form-control" id="floatingInput" name="username" placeholder="Username">
+            <label for="floatingInput">Username</label>
+            </div>
+            <div class="form-floating mb-3 w-100">
+            <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            </div>
+            <button class=" btn btn-lg btn-primary" type="submit">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+        </form>
+    </main>
+</body>
+</html>
---
+++ admin/signin.php
@@ -0,0 +1,9 @@
+<?php
+if(!empty($_POST['username']) && !empty($_POST['password'])){
+
+}
+else(
+    header("location: index.php
+    ");
+)
+?>
